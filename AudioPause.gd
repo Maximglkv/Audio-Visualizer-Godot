b348f0bb9c0bc389d2f9ed7af2cfdfb6be55840b
Maximglkv <91562210+Maximglkv@users.noreply.github.com>
2024-12-18 19:49:07 +0000

    removing random crap

--- AudioPause.gd
+++ AudioPause.gd
@@ -1,15 +1,4 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-	
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_toggled(toggled_on: bool) -> void:
 	AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), toggled_on)
